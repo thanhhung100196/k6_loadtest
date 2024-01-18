1ccfb530d0089b41cf2850c38b371ada05caca66
thanhhung100196 <thanhhung100196@gmail.com>
2024-01-18 10:40:55 +0700

    fix duplicate sleep

--- demo.js
+++ demo.js
@@ -1,9 +1,5 @@
 import { sleep } from 'k6/http';
 
-// This code imports the 'http' and 'sleep' modules from the 'k6' library.
-// It defines a sleep(1);
-sleep(1);
-import { sleep } from 'k6';
 
 export default function () {
   const url = 'https://alpha.conative.ai/sign-in';
