cdb41e2b6acfd673210e18d8d945ade0f2b26ff8
thanhhung100196 <thanhhung100196@gmail.com>
2024-01-18 10:23:38 +0700

    rename js

--- demo.js
+++ demo.js
@@ -1,3 +1,5 @@
+// This code imports the 'http' and 'sleep' modules from the 'k6' library.
+// It defines a default function that sends an HTTP GET request to 'https://test.k6.io' and then sleeps for 1 second.
 import http from 'k6/http';
 import { sleep } from 'k6';
 
